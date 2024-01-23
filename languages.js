c9ee1318556c2b92141bb45a8249b7dcdbddb1d3
Rehaan Ansari <90333544+rehaanansari811@users.noreply.github.com>
2024-01-23 19:21:04 +0530

    initial commit

---
+++ languages.js
@@ -0,0 +1,15 @@
+const languages = [
+  {
+    no: "27",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },
+  {
+    no: "16",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+
+];
